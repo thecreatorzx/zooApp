fe7172df527a56808cc236b6c9a2612b229f3184
thecreatorzx <saadyesican@gmail.com>
2024-01-25 20:23:56 +0530

    added a little

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,10 +4,19 @@ const Books = () => {
   return (
     <div className="book-box">
       <div className="book">
-        <div className="coverLeft1"></div>
+        <div className="coverLeft1">Hello my name is mohd saad</div>
         <div className="coverLeft2"></div>
+        <div className="page1Left"></div>
+        <div className="page1Right"></div>
+        <div className="page2Left"></div>
+        <div className="page2Right"></div>
+        <div className="page3Left"></div>
+        <div className="page3Right"></div>
         <div className="coverRight1"></div>
-        <div className="coverRight2"></div>
+        <div className="coverRight2">
+          <div className="endSide"></div>
+          <div className="endBottom"></div>
+        </div>
       </div>
     </div>
   );
